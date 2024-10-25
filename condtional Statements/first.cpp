f6ca1753381ac12601992e220327603e136cfabf
Bhanu Priya <bhanurathore099@gmail.com>
2024-10-25 12:54:38 +0530

    changes

--- condtional Statements/first.cpp
+++ condtional Statements/first.cpp
@@ -1,84 +1,8 @@
+// this is the first basic program. 
 #include <iostream>
 using namespace std;
 
 int main() {
-   int n = -45;
-
-   if (n >= 0) {   //return true value or false
-    cout << "n us positive\n";
-   
-   } else {
-   
-   cout << "n is negative\n";
-    
-    }
-   
-   
-    return 0;
-}
-
-
-// prblm: check age
-
-#include <iostream>
-using namespace std;
-
-int main() {
-   int age;
-   cout << "Enter your age: ";
-   cin >> age;
-
-   if(age >= 18) {    // if -> true (+ve)
-    cout << "You can vote\n";
-   } else {          // else -> false (-ve)
-
-    cout << "You cannot vote\n";
-   }
-
-     return 0;
-}
-
-
-// check odd or even 
-#include <iostream>
-using namespace std;
-
-int main() {
-   int n;
-   cout << "Enter number: ";
-   cin >> n;
-
-   if (n%2 == 0) {
-    cout << "even";
-   } else {
-
-    cout << "odd";
-
-   }
-   
-
-     return 0;
-}
-
-// problem : check grades of a student 
-// if-else if-else (conditional statement)
-#include <iostream>
-using namespace std;
-
-int main() {
-    int marks;
-    cout << "Enter your Marks: ";
-    cin >> marks;
-
-    if (marks >= 90) {
-        cout << "A\n";
-    } else if (marks >=80 && marks < 90) {  // use logical operator here &&
-        cout << "B\n";
-    } else  {
-        cout << "c\n";
-    }
-    
-    
-     return 0;
-
+   cout << "i am bhanu rathore\n from jammu";
+   return 0;
 }
